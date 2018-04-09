60755aebb3152857ea5f7979742ff675481d8831
renato1451 <38025564+renato1451@users.noreply.github.com>
2018-04-09 02:18:02 -0500

    Update main.cpp

--- ejercicio 10/main.cpp
+++ ejercicio 10/main.cpp
@@ -5,9 +5,9 @@ int str2int(char *str)
 {
     int res = 0; //inicializamos el resultado
     for (int i = 0; str[i] != '\0'; ++i)
-        res = res*10 + str[i] - '0';
+        res = res*10 + str[i] - '0'; // hacemos una convercion del string a entero atravez del codigo ascii
 
-    // return result.
+    // retornamos el resultado.
     return res;
 }
 
@@ -16,18 +16,18 @@ float str2float(char *x)
 	float total=0, num;
 	int df=10,i=0;
 	bool decimal=false;
-	while(x[i])
+	while(x[i])            // mientras x tenga elementos
 	{
-		num=x[i]-'0';
+		num=x[i]-'0';  // a la diferencia de codigo ascii 
 		if(decimal)
 		{
-			num=num/df;
+			num=num/df;   
 			df=df*10;
 		}
 		else if (!decimal && ( x[i]>='0' && x[i]<='9'))
 			total=total*10;
 
-		if(x[i]=='.' || x[i]==',' )
+		if(x[i]=='.' || x[i]==',' ) //busca la coma  o el punto para saber si es un numero decimal
 			decimal=true;
 		else total+=num;
 
@@ -35,7 +35,7 @@ float str2float(char *x)
 	}
 return total;
 }
-bool isDecimal(char *x)
+bool isDecimal(char *x) //funcion booleana que devuelve true si ecuentra una coma o un punto
 {
     bool decimal=false;
     int i=0;
@@ -47,18 +47,18 @@ bool isDecimal(char *x)
     }
     return decimal;
 }
-void isIntorFloat(char *x)
+void isIntorFloat(char *x) //hace la conversion de cualquier numero a un numero decimal o a un numero entero
 {
    if (isDecimal(x)==true)
    {
        cout<<str2float(x);
    }
-   else
+   else                         
    {
        cout<<str2int(x);
    }
 }
-// Driver program to test above function
+
 int main()
 {
     char str[100];
