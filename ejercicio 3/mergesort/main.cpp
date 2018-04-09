e54531cd52b793da9c10422931dbac682f1813c4
renato1451 <38025564+renato1451@users.noreply.github.com>
2018-04-09 01:45:14 -0500

    Update main.cpp

--- ejercicio 3/mergesort/main.cpp
+++ ejercicio 3/mergesort/main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void merge(int a[], int startIndex, int endIndex)
 {
 
-int size = (endIndex - startIndex) + 1;
-int *b = new int [size]();
+int size = (endIndex - startIndex) + 1; //decimos que size es igual al indice final menos el inicial
+int *b = new int [size]();  // inicializamos el puntero a entero b con memoria dinamica que seria una matriz de combinacion temporal
 
-int i = startIndex;
-int mid = (startIndex + endIndex)/2;
-int k = 0;
-int j = mid + 1;
+int i = startIndex;             // i es para la izquierda
+int mid = (startIndex + endIndex)/2; // el medio esta declarado como la suma del (inicio mas el final)/2
+int k = 0;              //k es para el array temporal
+int j = mid + 1;        // j es para la derecha
 
 while (k < size)
 {
@@ -35,7 +35,7 @@ delete []b;
 }
 
 //The recursive merge sort function
-void merge_sort(int iArray[], int startIndex, int endIndex)
+void merge_sort(int iArray[], int startIndex, int endIndex) 
 {
 int midIndex;
 
@@ -45,15 +45,15 @@ if (startIndex >= endIndex)
     return;
 }
 
-//First, divide in half
+//Primero divide el arreglo en 2 partes
 midIndex = (startIndex + endIndex)/2;
 
-//First recursive call
-merge_sort(iArray, startIndex, midIndex);
-//Second recursive call
-merge_sort(iArray, midIndex+1, endIndex);
-//The merge function
-merge(iArray, startIndex, endIndex);
+//Primera llamada recursiva
+merge_sort(iArray, startIndex, midIndex); //recorre la izquierda
+//segunda llamada recursiva
+merge_sort(iArray, midIndex+1, endIndex); // recorre la deracha
+//Tercera llamada recursiva
+merge(iArray, startIndex, endIndex); 
 
 }
 
