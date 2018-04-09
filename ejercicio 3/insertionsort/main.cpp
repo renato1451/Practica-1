67eaf61609f7a8298bc9baaf75cf06605a4d884c
renato1451 <38025564+renato1451@users.noreply.github.com>
2018-04-09 01:34:09 -0500

    Update main.cpp

--- ejercicio 3/insertionsort/main.cpp
+++ ejercicio 3/insertionsort/main.cpp
@@ -3,83 +3,22 @@
 #include <cstdlib>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void mostrar(int arr[],int size)
-{
-	for(int i=0;i<size;i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-}
 void insertionSort(int arr[],int size)
 {
 	int j,temp;
 	for(int i=0;i<size;i++)
 	{
 		j=i;
-		while(j>0 && arr[j-1]>arr[j])
+		while(j>0 && arr[j-1]>arr[j])// comenzamos desde j=1 y siempre y cuando que arr[j-1]>arr[j] haremos un intercambio
 		{
-			temp=arr[j];
-			arr[j]=arr[j-1];
-			arr[j-1]=temp;
-			j--;
+			temp=arr[j];  // guardamos a arr[j]=en temp
+			arr[j]=arr[j-1];//le asignamos un nuevo valor a arr[j] que es arr[j-1]
+			arr[j-1]=temp;  // y arr[j-1] le asignamos el valor de temp
+			j--;           //y disminuye j;
 		}
 	}
 }
-void merge(int *arr,int *p,int *q,int sizep,int sizeq)
-{
-	int i=0,j=0,k=0;
-	while(i<sizep && j<sizeq)
-	{
-		 if(p[i]<=q[j])
-		 {
-		 	arr[k]=p[i];
-		 	k++;
-		 	i++;
-		 }
-		 else
-		 {
-		 	arr[k]=q[j];
-		 	k++;
-		 	j++;
-		 }
-	}
-	while(i<sizep)
-	{
-		arr[k]=p[i];
-		k++;i++;
-	}
-	while(j<sizeq)
-	{
-		arr[k]=p[j];
-		k++;j++;
-	}
-}
-void mergeSort(int *arr,int size)
-{
-	if(size==1)
-	{
-		return;
-	}
-	int inicio=0;
-	int final=inicio + size -1 ;
-	int medio=(inicio + final)/2;
-	int sizep=medio-inicio + 1;
-	int sizeq=final-medio;
-	int *p=new int[sizep];
-	int *q=new int[sizeq];
-	for(int i=inicio;i<=medio;i++)
-	{
-		p[i]=arr[i];
-	}
-	for(int i=medio + 1;i<=final;i++)
-	{
-		q[i-medio-1]=arr[i];
-	}
-	mergeSort(q,sizeq);
-	mergeSort(p,sizep);
-	merge(arr,p,q,sizep,sizeq);
-	
-}
+
 int main(int argc, char** argv) {
 	int size;
 	cin>>size;
@@ -90,7 +29,6 @@ int main(int argc, char** argv) {
 	}
 	cout<<endl;
 	//insertionSort(numeros,size);
-	//mostrar(numeros,size);
 	mergeSort(numeros,size);
 	for(int i=0;i<size;i++)
 	{
